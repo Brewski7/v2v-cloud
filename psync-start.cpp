43350c8e46e8ab93f37d01757ede166ad0d2df6e
brewski <22786171@sun.ac.za>
2025-07-25 02:09:28 +0200

    Msg output

--- psync-start.cpp
+++ psync-start.cpp
@@ -328,7 +328,7 @@ private:
 
   void executeCommand(const std::string& filepath)
   {
-    std::cout << termcolor::on_blue << termcolor::white << "Executing /cmd...\n" << termcolor::reset << std::endl;
+    std::cout << termcolor::on_blue << termcolor::white << "Executing /cmd..." << termcolor::reset << "\n" << std::endl;
 
     //std::string chmodCmd = "chmod +x " + filepath;
     //std::system(chmodCmd.c_str());
